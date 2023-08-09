e097e5bb805ae34b281c43ed3678601826e210c0
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2023-08-09 22:49:35 +0530

    feat: implemented reset password via email

--- backend/settings.py
+++ backend/settings.py
@@ -158,3 +158,13 @@ REST_FRAMEWORK ={
     #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
+
+# SMTP Setup
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True 
+EMAIL_PORT = 587 
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+FRONTEND_URL = 'https://your-frontend-url.com' 
